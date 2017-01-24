f396fa3eec8ffe4b9f3568013a2d5ff44560ab5d
thurd <thurd@B11-R111E-09>
2017-01-24 13:22:43 -0800

    Added stuff.

--- BranchTest/BranchTest/Form1.Designer.cs
+++ BranchTest/BranchTest/Form1.Designer.cs
@@ -29,7 +29,8 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtStuff = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -37,23 +38,33 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Stuff";
             // 
-            // textBox1
+            // txtStuff
             // 
-            this.textBox1.Location = new System.Drawing.Point(141, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtStuff.Location = new System.Drawing.Point(70, 13);
+            this.txtStuff.Name = "txtStuff";
+            this.txtStuff.Size = new System.Drawing.Size(131, 20);
+            this.txtStuff.TabIndex = 1;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(70, 39);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(216, 71);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtStuff);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -65,7 +76,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtStuff;
+        private System.Windows.Forms.Button btnGo;
     }
 }
 
